e584eae2d005f9ef752cdb3e80fb12de0d2ace4f
Alina Petrova <60265738+Alina-Petrova@users.noreply.github.com>
2021-01-20 11:33:10 +0100

    modifica

--- src/esercizio1/App.java
+++ src/esercizio1/App.java
@@ -17,27 +17,33 @@ public class App {
         VeicoloMotore v;
         String tipo = "";
         
-        Auto a = new Auto(VeicoloMotore.Marca.Audi, 2012, 1000, Auto.Alimentazione.BENZINA, 4);
-        Garage.immissioneVeicolo(a);
-        Moto m = new Moto(VeicoloMotore.Marca.Honda, 2020, 500, 4);
-        Garage.immissioneVeicolo(m);
-        Furgone f = new Furgone(VeicoloMotore.Marca.Fiat, 2013, 1500, 85);
-        Garage.immissioneVeicolo(f);
-        a = new Auto(VeicoloMotore.Marca.Ford, 2019, 1000, Auto.Alimentazione.GPL, 5);
-        Garage.immissioneVeicolo(a);
+        try {
+            
+            Garage g = new Garage(7);
+            Auto a = new Auto(VeicoloMotore.Marca.Audi, 2012, 1000, Auto.Alimentazione.BENZINA, 4);
+            g.immissioneVeicolo(a);
+            Moto m = new Moto(VeicoloMotore.Marca.Honda, 2020, 500, 4);
+            g.immissioneVeicolo(m);
+            Furgone f = new Furgone(VeicoloMotore.Marca.Fiat, 2013, 1500, 85);
+            g.immissioneVeicolo(f);
+            a = new Auto(VeicoloMotore.Marca.Ford, 2019, 1000, Auto.Alimentazione.GPL, 5);
+            g.immissioneVeicolo(a);
+
+            System.out.println(g.situazioneGarage());
+
+            v = g.estrazioneVeicolo(1);
+
+            if(v instanceof Auto)
+                tipo = "Auto ";
+            if(v instanceof Moto)
+                tipo = "Moto ";
+            if(v instanceof Furgone)
+                tipo = "Furgone ";
+            System.out.println("Uscito "+tipo+v.toString());
+            System.out.println(g.situazioneGarage());
         
-        System.out.println(Garage.situazioneGarage());
-        
-        v = Garage.estrazioneVeicolo(1);
-        
-        if(v instanceof Auto)
-            tipo = "Auto ";
-        if(v instanceof Moto)
-            tipo = "Moto ";
-        if(v instanceof Furgone)
-            tipo = "Furgone ";
-        System.out.println("Uscito "+tipo+v.toString());
-        System.out.println(Garage.situazioneGarage());
-        
-    }
+        }catch (Exception e) {
+            System.out.println("Oops qualcosa è andato storto...");
+        }
+    }    
 }
--- src/esercizio1/Garage.java
+++ src/esercizio1/Garage.java
@@ -6,29 +6,54 @@
 package esercizio1;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author utente
  */
 public class Garage {
-    static ArrayList<VeicoloMotore> elencoVeicoli = new ArrayList<VeicoloMotore>();
-    
-    
-    
-    public static void immissioneVeicolo(VeicoloMotore veicolo) {
-        elencoVeicoli.add(veicolo);
+    private final List<VeicoloMotore> parcheggio;
+    private final int posti;
+
+    public Garage(int posti) {
+        this.posti = posti;
+        parcheggio = new ArrayList<VeicoloMotore>(posti);
+        inizializza();
     }
     
-    public static VeicoloMotore estrazioneVeicolo(int index) {
-        elencoVeicoli.remove(index);
-        return elencoVeicoli.get(index);           
+    private int postoLibero() {
+        return parcheggio.indexOf(null);  
+    } 
+    private void inizializza() {
+        for(int i=0; i<posti; i++)
+            parcheggio.add(null);
+    }
+
+    public void immissioneVeicolo(VeicoloMotore veicolo) {
+        if (postoLibero() == -1) {
+            throw new GarageException("Il garage è pieno...");
+        }
+        parcheggio.set(postoLibero(), veicolo);
     }
     
-    public static String situazioneGarage() {
+    public VeicoloMotore estrazioneVeicolo(int index) {
+        if (index >= this.posti || parcheggio.get(index) == null) {
+            throw new GarageException("Il posto non esiste oppure è vuoto");
+        }
+        VeicoloMotore v = parcheggio.get(index);
+        parcheggio.set(index, null);
+        return v;           
+    }
+    
+    public String situazioneGarage() {
         String ris = "";        
-        for(VeicoloMotore v: elencoVeicoli) {
-            ris += v.toString();
+        for (int i=0; i< parcheggio.size(); i++) {
+            if (parcheggio.get(i) != null) {
+                ris+="--------------- POSTO " + i + "------------------";
+                ris+=parcheggio.get(i)+"\n---------------------------------\n";
+            }
+
         }
         return ris;
     }
